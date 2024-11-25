409df127f794ca5e1c94b91b798196a07fddd72c
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-11-25 11:51:41 +0100

    started adding a workout card on homescreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,11 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { Button, Card } from 'react-native-paper';
+import { View, Image, StyleSheet } from 'react-native';
+import { Avatar, Button, Card, Text } from 'react-native-paper';
 
 export default function HomeScreen() {
     const nickname = 'Janas';
+    const workoutDate = new Date();
+    const formattedDate = `${workoutDate.getDate()} ${workoutDate.toLocaleString('default', { month: 'long' })} ${workoutDate.getFullYear()}`;
+    const LeftContent = props => <Avatar.Icon {...props} icon='folder' />;
 
     return (
         <View style={{ flex: 1, padding: 15, gap: 15 }}>
@@ -18,11 +21,18 @@ export default function HomeScreen() {
             <Text style={{ fontSize: 16, fontWeight: 'bold', color: 'grey' }}>Recent workouts</Text>
 
             <Card>
-                <Text>Workout</Text>
-                <Text>Workout</Text>
-                <Text>Workout</Text>
-                <Text>Workout</Text>
-                <Text>Workout</Text>
+                <Card.Content>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Workout</Text>
+                    <Text style={{ fontSize: 12, fontWeight: 'bold', color: 'grey' }}>
+                        {formattedDate}
+                    </Text>
+                </Card.Content>
+                <Card.Title title='Card Title' subtitle='Card Subtitle' left={LeftContent} />
+                <Card.Title title='Card Title' subtitle='Card Subtitle' left={LeftContent} />
+                <Card.Actions>
+                    <Button>Cancel</Button>
+                    <Button>Ok</Button>
+                </Card.Actions>
             </Card>
         </View>
     );
@@ -31,7 +41,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     button: {
         backgroundColor: '#1778f2',
-        paddingVertical: 15,
+        paddingVertical: 13,
         borderRadius: 5,
         elevation: 24
     },
