f0b6cc32b13af01fed970ef9e94861c5e4d6141c
rafal <you@example.com>
2024-12-09 11:29:28 +0100

    split register component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -2,55 +2,14 @@ import React, { useState } from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 import { Text, TextInput, Button } from 'react-native-paper';
 import Logo from 'src/components/Logo';
+import RegisterFields from 'src/components/Register';
 import stylesGlobal from 'src/styles/style';
 
 function Register({ navigation }: any) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [password2, setPassword2] = useState('');
-
-    const handleUsername = (text: string) => {
-        setUsername(text);
-    };
-
-    const handlePassword = (text: string) => {
-        setPassword(text);
-    };
-
-    const handlePassword2 = (text: string) => {
-        setPassword2(text);
-    };
-
-    const onRegister = () => {
-        console.log('Register');
-    };
-
     return (
         <View style={styles.container}>
             <Logo></Logo>
-            <TextInput
-                label='username'
-                value={username}
-                onChangeText={text => handleUsername(text)}
-                style={styles.textInput}
-            />
-            <TextInput
-                label='password'
-                secureTextEntry
-                value={password}
-                onChangeText={text => handlePassword(text)}
-                style={styles.textInput}
-            />
-            <TextInput
-                label='coonfirm password'
-                secureTextEntry
-                value={password}
-                onChangeText={text => handlePassword(text)}
-                style={styles.textInput}
-            />
-            <Button onPress={onRegister} mode='contained' style={styles.button}>
-                Register
-            </Button>
+            <RegisterFields></RegisterFields>
             <View style={styles.bottomTextContainer}>
                 <Text style={styles.bottomText}>Already have an account?</Text>
                 <Text
