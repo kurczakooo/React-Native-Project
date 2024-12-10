44fa54fc20c165021fbca83e3c8bd047941b4894
Filip Stępień <filip.stepien30@gmail.com>
2024-12-10 17:12:07 +0100

    add eslint rule to allow overriding global type

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ['expo', 'prettier'],
     plugins: ['prettier'],
     rules: {
-        '@typescript-eslint/no-unused-vars': 'off'
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-empty-object-type': 'off'
     }
 };
