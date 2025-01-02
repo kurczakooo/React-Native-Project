b9ab615deb80997874975244dad4068345cd47e7
rafal <you@example.com>
2025-01-02 12:17:12 +0100

    fix regression

--- src/contexts/userIdContext.ts
+++ src/contexts/userIdContext.ts
@@ -1,8 +1,9 @@
 import { createContext, useContext } from 'react';
 
 export const userIdContext = createContext<{
-    userId: string;
-    setUserId: React.Dispatch<React.SetStateAction<string>>;
+    // null indicates that user IS NOT LOG IN! YOU SHALL NOT introduce regression features by chaning type to string
+    userId: string | null;
+    setUserId: React.Dispatch<React.SetStateAction<string | null>>;
 }>({
     userId: '',
     setUserId: () => ''
