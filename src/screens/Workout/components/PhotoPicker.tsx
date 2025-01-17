2f2dc5ff68f3da0fe4af3ef0be1102a0485a0b0d
Filip Stępień <filip.stepien30@gmail.com>
2025-01-17 23:30:44 +0100

    tidy up picker state

--- src/screens/Workout/components/PhotoPicker.tsx
+++ src/screens/Workout/components/PhotoPicker.tsx
@@ -1,60 +1,30 @@
-import { useState } from 'react';
-import { MediaType, launchImageLibraryAsync, launchCameraAsync } from 'expo-image-picker';
 import React from 'react';
 import PhotoPickButton from './PhotoPickButton';
 import PhotoPickDialog from './PhotoPickDialog';
 
 type PhotoPickerProps = {
-    onUriUpdate: (uri: string) => void;
+    imageUri: string;
+    dialogVisible: boolean;
+    onDialogCancel?: () => void;
+    onButtonClick?: () => void;
+    onMediaDelete?: () => void;
+    onImagePick?: () => void;
+    onPhotoPick?: () => void;
 };
 
-async function getMediaUri(source: 'images' | 'camera') {
-    const options: { mediaTypes: MediaType[]; quality: number } = {
-        mediaTypes: ['images'],
-        quality: 1
-    };
-
-    const result =
-        source === 'images'
-            ? await launchImageLibraryAsync(options)
-            : await launchCameraAsync(options);
-
-    return result.assets?.at(0)?.uri ?? '';
-}
-
 export default function PhotoPicker(props: PhotoPickerProps) {
-    const [dialogVisible, setDialogVisible] = useState(false);
-    const [imageUri, setImageUri] = useState('');
-    const { onUriUpdate } = props;
-
-    const showDialog = () => {
-        setDialogVisible(true);
-    };
-
-    const hideDialog = () => {
-        setDialogVisible(false);
-    };
-
-    const handleMediaPick = async (source: 'images' | 'camera') => {
-        const uri = await getMediaUri(source);
-        onUriUpdate(uri);
-        setImageUri(uri);
-        hideDialog();
-    };
-
-    const handleMediaDelete = () => {
-        onUriUpdate('');
-        setImageUri('');
-    };
-
     return (
         <>
-            <PhotoPickButton onPick={showDialog} onDelete={handleMediaDelete} imageUri={imageUri} />
+            <PhotoPickButton
+                onPick={props.onButtonClick}
+                onDelete={props.onMediaDelete}
+                imageUri={props.imageUri}
+            />
             <PhotoPickDialog
-                visible={dialogVisible}
-                onCancel={hideDialog}
-                onPickImage={() => handleMediaPick('images')}
-                onTakePhoto={() => handleMediaPick('camera')}
+                visible={props.dialogVisible}
+                onCancel={props.onDialogCancel}
+                onPickImage={props.onImagePick}
+                onTakePhoto={props.onPhotoPick}
             />
         </>
     );
--- src/screens/Workout/components/WorkoutInfoCard.tsx
+++ src/screens/Workout/components/WorkoutInfoCard.tsx
@@ -1,11 +1,55 @@
+import { useState } from 'react';
+import { MediaType, launchImageLibraryAsync, launchCameraAsync } from 'expo-image-picker';
 import { StyleSheet } from 'react-native';
 import PhotoPicker from './PhotoPicker';
 import Card from 'src/components/Card';
 
+async function getMediaUri(source: 'images' | 'camera') {
+    const options: { mediaTypes: MediaType[]; quality: number } = {
+        mediaTypes: ['images'],
+        quality: 1
+    };
+
+    const result =
+        source === 'images'
+            ? await launchImageLibraryAsync(options)
+            : await launchCameraAsync(options);
+
+    return result.assets?.at(0)?.uri ?? '';
+}
+
 export default function WorkoutInfoCard() {
+    const [dialogVisible, setDialogVisible] = useState(false);
+    const [imageUri, setImageUri] = useState('');
+
+    const showDialog = () => {
+        setDialogVisible(true);
+    };
+
+    const hideDialog = () => {
+        setDialogVisible(false);
+    };
+
+    const handleMediaPick = async (source: 'images' | 'camera') => {
+        setImageUri(await getMediaUri(source));
+        hideDialog();
+    };
+
+    const handleMediaDelete = () => {
+        setImageUri('');
+    };
+
     return (
         <Card>
-            <PhotoPicker onUriUpdate={console.log} />
+            <PhotoPicker
+                dialogVisible={dialogVisible}
+                imageUri={imageUri}
+                onButtonClick={showDialog}
+                onDialogCancel={hideDialog}
+                onMediaDelete={handleMediaDelete}
+                onImagePick={() => handleMediaPick('images')}
+                onPhotoPick={() => handleMediaPick('camera')}
+            />
         </Card>
     );
 }
