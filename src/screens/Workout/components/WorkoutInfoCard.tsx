921bdc87b5c71b5b5093065349036ac0df8477f9
Filip Stępień <filip.stepien30@gmail.com>
2025-01-17 17:13:48 +0100

    started refactoring WorkoutCard

---
+++ src/screens/Workout/components/WorkoutInfoCard.tsx
@@ -0,0 +1,8 @@
+import { Pressable, StyleSheet, View, Image } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import ThemedIcon from 'src/components/ThemedIcon';
+import { Theme } from 'src/types';
+
+export default function WorkoutInfoCard() {}
+
+const styles = StyleSheet.create({});
